5d128d80b59cf7a0322238cb49b0525f1e3d08dc
Nico Schmoigl <github@schmoigl-online.de>
2016-09-24 22:30:02 +0200

    Icon and text of multiple messages added
    Button to hide it still is missing

--- src/main/java/org/jenkinsci/plugins/systemmessage/model/MessageLevel.java
+++ src/main/java/org/jenkinsci/plugins/systemmessage/model/MessageLevel.java
@@ -1,16 +1,18 @@
 package org.jenkinsci.plugins.systemmessage.model;
 
 public enum MessageLevel {
-	INFORMATION(Messages.MessageLevel_INFORMATION(), "org-jenkinsci-plugins-systemmessage-information"),
-	WARNING(Messages.MessageLevel_WARNING(), "org-jenkinsci-plugins-systemmessage-warning"),
-	FATAL(Messages.MessageLevel_FATAL(), "org-jenkinsci-plugins-systemmessage-fatal");
+	INFORMATION(Messages.MessageLevel_INFORMATION(), "org-jenkinsci-plugins-systemmessage-information", "images/info.png"),
+	WARNING(Messages.MessageLevel_WARNING(), "org-jenkinsci-plugins-systemmessage-warning", "images/warning.png"),
+	FATAL(Messages.MessageLevel_FATAL(), "org-jenkinsci-plugins-systemmessage-fatal", "images/fatal.png");
 	
 	private final String text;
 	private final String cssClass;
+	private final String iconPath;
 	
-	private MessageLevel(String text, String cssClass) {
+	private MessageLevel(String text, String cssClass, String iconPath) {
 		this.text = text;
 		this.cssClass = cssClass;
+		this.iconPath = iconPath;
 	}
 
 	public String getText() {
@@ -19,5 +21,9 @@ public enum MessageLevel {
 
 	public String getCSSClass() {
 		return this.cssClass;
+	}
+	
+	public String getIconPath() {
+		return this.iconPath;
 	}
 }
